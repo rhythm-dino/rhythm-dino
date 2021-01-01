06efedcb2a5e6d8fc0671cf4612127388b1d0e38
yuzijiangorz <61646780+yuzijiangorz@users.noreply.github.com>
2021-01-01 22:38:51 +0800

    Update Main.java

--- src/dino/main/Main.java
+++ src/dino/main/Main.java
@@ -10,14 +10,6 @@ public class Main {
 
 
     }
-    public static void audioTest() {
-        audio player = new audio("/home/amiriox/Music/CloudMusic/findthepieces.mp3");
-        player.play();
-    }
-    public static void main(String[] args) throws InterruptedException, IOException {
-        imgPalTest();
-        Process process = Runtime.getRuntime().exec("vlc /home/amiriox/Music/CloudMusic/song.wav");
-    }
 }
 
 
