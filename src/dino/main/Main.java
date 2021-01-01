7582cfdbe917b6d3616a880898c7443f12a12739
AMIRIOX <wumingyun2120@outlook.com>
2021-01-01 10:19:52 +0800

    [test] image display test done.

--- src/dino/main/Main.java
+++ src/dino/main/Main.java
@@ -5,79 +5,89 @@ import dino.extension.*;
 import java.util.*;
 
 public class Main {
-
+    public static void imgPalTest() {
+        JFrame frame = new JFrame("Image Panel Test");
+        ImageShower imageShower = new ImageShower();
+        imageShower.addFileToCache("/disk01/resource/silk/1.png", "imgTester");
+        imageShower.displayImage("imgTester", frame, new position(100,100), 500, 600);
+        frame.pack();
+        frame.setVisible(true);
+    }
     public static void main(String[] args) {
-        Map<String,Object> inif=new Map<String, Object>() {
-            @Override
-            public int size() {
-                return 0;
-            }
 
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean containsKey(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsValue(Object o) {
-                return false;
-            }
-
-            @Override
-            public Object get(Object o) {
-                return null;
-            }
-
-            @Override
-            public Object put(String s, Object o) {
-                return null;
-            }
-
-            @Override
-            public Object remove(Object o) {
-                return null;
-            }
-
-            @Override
-            public void putAll(Map<? extends String, ?> map) {
-
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public Set<String> keySet() {
-                return null;
-            }
-
-            @Override
-            public Collection<Object> values() {
-                return null;
-            }
-
-            @Override
-            public Set<Entry<String, Object>> entrySet() {
-                return null;
-            }
-        };
-        try {
-            inif=readini.readIni("/home/caviarx/桌面/tests.ini");
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        Set s=inif.keySet();
-        Iterator i=s.iterator();
-        while (i.hasNext()){
-            Object a=i.next();
-            System.out.println("key: "+a+'\n'+"value: "+inif.get(a));
-        }
+        // @yuzijiangorz: 不要把测试代码直接写到main里面 写个xxxTest方法(懒得写类了)然后main调用!
+        imgPalTest();
+//        Map<String,Object> inif=new Map<String, Object>() {
+//            @Override
+//            public int size() {
+//                return 0;
+//            }
+//
+//            @Override
+//            public boolean isEmpty() {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean containsKey(Object o) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean containsValue(Object o) {
+//                return false;
+//            }
+//
+//            @Override
+//            public Object get(Object o) {
+//                return null;
+//            }
+//
+//            @Override
+//            public Object put(String s, Object o) {
+//                return null;
+//            }
+//
+//            @Override
+//            public Object remove(Object o) {
+//                return null;
+//            }
+//
+//            @Override
+//            public void putAll(Map<? extends String, ?> map) {
+//
+//            }
+//
+//            @Override
+//            public void clear() {
+//
+//            }
+//
+//            @Override
+//            public Set<String> keySet() {
+//                return null;
+//            }
+//
+//            @Override
+//            public Collection<Object> values() {
+//                return null;
+//            }
+//
+//            @Override
+//            public Set<Entry<String, Object>> entrySet() {
+//                return null;
+//            }
+//        };
+//        try {
+//            inif=readini.readIni("/home/caviarx/桌面/tests.ini");
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        Set s=inif.keySet();
+//        Iterator i=s.iterator();
+//        while (i.hasNext()){
+//            Object a=i.next();
+//            System.out.println("key: "+a+'\n'+"value: "+inif.get(a));
+//        }
     }
 }
