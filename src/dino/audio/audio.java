c9a472db11c39ffc42e429beb62f2105310589cc
AMIRIOX <wumingyun2120@outlook.com>
2021-01-01 14:57:25 +0800

    [feature] audio player by yzj

--- src/dino/audio/audio.java
+++ src/dino/audio/audio.java
@@ -1,36 +1,13 @@
 package dino.audio;
 import java.io.*;
-<<<<<<< HEAD
-import javax.media.*;
-=======
 import javazoom.jl.player.*;
 import javazoom.jl.decoder.*;
 import javazoom.jl.converter.*;
->>>>>>> 7dd1954 (audio finished)
 class audioException extends Exception{
     audioException(String errMesage){
         super(errMesage);
     }
 }
-<<<<<<< HEAD
-public class audio {
-
-    private String address;
-    private Player player;
-    public audio(String a){
-        this.address = a;
-    }
-    public void start() throws IOException,NoPlayerException{
-        File file=new File(address);
-        player = Manager.createPlayer(file.toURL());
-        player.start();
-    }
-    public void pause() throws NoPlayerException{
-        player.stop();
-    }
-    public void unpause() throws NoPlayerException{
-        player.start();
-=======
 class AudioMultiThread implements Runnable{
     private String addr;
     AudioMultiThread(String address){
@@ -71,6 +48,6 @@ public class audio{
     }
     public void end(){
         t.interrupt();
->>>>>>> 7dd1954 (audio finished)
     }
 }
+
--- src/dino/main/Main.java
+++ src/dino/main/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void imgPalTest() throws InterruptedException {
         JFrame frame = new JFrame("Image Panel Test");
         ImageShower imageShower = new ImageShower();
-        imageShower.addFileToCache("/disk01/resource/silk/1.png", "imgTester");
-        imageShower.displayImage("imgTester", frame, new position(100,500), 500, 600);
+        imageShower.addFileToCache("/disk01/resource/home/VisualStudioBackground/hengzhe.jpg", "imgTester");
+        imageShower.displayImage("imgTester", frame, new position(400,500), 1200, 500);
 //        imageShower.hideImage("imgTester");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
@@ -26,81 +26,81 @@ public class Main {
 
         }
     }
-    public static void main(String[] args) throws InterruptedException {
+    public void sthtest() {
+                Map<String,Object> inif=new Map<String, Object>() {
+            @Override
+            public int size() {
+                return 0;
+            }
 
-        // @yuzijiangorz: 不要把测试代码直接写到main里面 写个xxxTest方法(懒得写类了)然后main调用!
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object o) {
+                return false;
+            }
+
+            @Override
+            public Object get(Object o) {
+                return null;
+            }
+
+            @Override
+            public Object put(String s, Object o) {
+                return null;
+            }
+
+            @Override
+            public Object remove(Object o) {
+                return null;
+            }
+
+            @Override
+            public void putAll(Map<? extends String, ?> map) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Set<String> keySet() {
+                return null;
+            }
+
+            @Override
+            public Collection<Object> values() {
+                return null;
+            }
+
+            @Override
+            public Set<Entry<String, Object>> entrySet() {
+                return null;
+            }
+        };
+        try {
+            inif=readini.readIni("/home/caviarx/桌面/tests.ini");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Set s=inif.keySet();
+        Iterator i=s.iterator();
+        while (i.hasNext()){
+            Object a=i.next();
+            System.out.println("key: "+a+'\n'+"value: "+inif.get(a));
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
         imgPalTest();
-//        Map<String,Object> inif=new Map<String, Object>() {
-//            @Override
-//            public int size() {
-//                return 0;
-//            }
-//
-//            @Override
-//            public boolean isEmpty() {
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean containsKey(Object o) {
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean containsValue(Object o) {
-//                return false;
-//            }
-//
-//            @Override
-//            public Object get(Object o) {
-//                return null;
-//            }
-//
-//            @Override
-//            public Object put(String s, Object o) {
-//                return null;
-//            }
-//
-//            @Override
-//            public Object remove(Object o) {
-//                return null;
-//            }
-//
-//            @Override
-//            public void putAll(Map<? extends String, ?> map) {
-//
-//            }
-//
-//            @Override
-//            public void clear() {
-//
-//            }
-//
-//            @Override
-//            public Set<String> keySet() {
-//                return null;
-//            }
-//
-//            @Override
-//            public Collection<Object> values() {
-//                return null;
-//            }
-//
-//            @Override
-//            public Set<Entry<String, Object>> entrySet() {
-//                return null;
-//            }
-//        };
-//        try {
-//            inif=readini.readIni("/home/caviarx/桌面/tests.ini");
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        Set s=inif.keySet();
-//        Iterator i=s.iterator();
-//        while (i.hasNext()){
-//            Object a=i.next();
-//            System.out.println("key: "+a+'\n'+"value: "+inif.get(a));
-//        }
     }
 }
