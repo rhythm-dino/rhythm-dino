c006763ff09baedf595d6423c9434af1c3e729a8
yuzijiangorz <yu520080505@163.com>
2021-01-04 08:50:28 +0800

    [feature] audio

--- src/dino/audio/audio.java
+++ src/dino/audio/audio.java
@@ -8,21 +8,29 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.*;
+import java.net.URI;
 //由于技术水平不足，使用ffmpeg将.mp3转换为.wav再播放
 public class audio{
     private Clip c;
     private File f;
     private AudioInputStream ais;
     private long pause_msec;
+    private Boolean istmp=false;
+    private String tmplocal=System.getProperty("user.dir")+"\\tmp\\poster.wav";
     public void Convert_mp3_to_wav(String mp3local,String wavLocal) throws Exception{
         Runtime.getRuntime().exec("ffplay -i "+mp3local+" -f wav "+wavLocal);
     }
     public void play(String musicPath) throws Exception{
         if(musicPath.contains(".mp3")){
-
+            Convert_mp3_to_wav(musicPath,tmplocal);
+            istmp=true;
+            play_wav(tmplocal);
         }
         else if(musicPath.contains(".wav")){
-
+            play_wav(musicPath);
+        }
+        else{
+            throw new audioException("This file does not contain mp3 or wav!");
         }
     }
     private void play_wav(String wavPath) throws Exception{
@@ -31,23 +39,40 @@ public class audio{
             ais=AudioSystem.getAudioInputStream(f);
             c=AudioSystem.getClip();
             c.open(ais);
-            c.start();
             c.loop(Clip.LOOP_CONTINUOUSLY);
+            c.start();
+
         }catch (Exception e){
             e.printStackTrace();
         }
     }
     public void pause() throws Exception{
-        pause_msec=c.getMicrosecondLength();
+        pause_msec=c.getMicrosecondPosition();
         c.stop();
     }
     public void unpause() throws Exception{
         c.setMicrosecondPosition(pause_msec);
+        c.loop(Clip.LOOP_CONTINUOUSLY);
         c.start();
     }
-    public void Reset() throws Exception{
+    public void reset() throws Exception{
         c.setMicrosecondPosition(0);
+        c.loop(Clip.LOOP_CONTINUOUSLY);
         c.start();
-    }
 
+    }
+    public void close() throws Exception{
+        c.close();
+        f=null;
+        ais=null;
+        pause_msec=0;
+        System.gc();
+        if(istmp){
+            if(! (new File(tmplocal).delete()) ){
+                throw new audioException("Error: Cannot delete tmp Files!");
+            }
+        }
+        istmp=false;
+        System.gc();
+    }
 }
--- src/dino/main/Main.java
+++ src/dino/main/Main.java
@@ -7,34 +7,28 @@ import java.util.concurrent.TimeUnit;
 import java.io.*;
 import dino.audio.*;
 public class Main {
-    public static void imgTest() {
-        JFrame frame = new JFrame("imgTest");
-        ImageShower imageShower = new ImageShower();
-        imageShower.addFileToCache("/disk01/resource/silk/1.png", "atest");
-        imageShower.displayImage("atest",frame,new position(500, 600), 600, 600);
 
-        frame.pack();
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    public static void adoTest() throws Exception {
+    public static void test() throws Exception{
+        audio a=new audio();
+        System.out.println("Start to play!");
+        a.play("C:\\x.wav");
+        Thread.sleep(9000);
+        System.out.println("Start to pause!");
 
+        System.out.println("start to resume!");
+        a.unpause();
+        Thread.sleep(2000);
+        System.out.println("start to reset !");
+        a.reset();
+        Thread.sleep(9000);
+        System.out.println("Program close!");
+        a.close();
     }
+
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
-//        imgTest();
-//        adoTest();
-//        Thread.sleep(10000000);
-        Scenes scenes = new Scenes();
-        JFrame frame = new JFrame("dinor test");
-        scenes.play(frame);
+       test();
 
-        frame.pack();
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-=======
-        adoTest();
->>>>>>> 93ed215 (...)
+
     }
 }
 
